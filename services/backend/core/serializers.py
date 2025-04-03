fcb1eca2b579b0c1e7f64eb880142a4eb3662836
4218 Пчеленко Е.Ю <e.pchelenko@gmail.com>
2025-04-03 15:07:03 +0300

    created serializers

---
+++ services/backend/core/serializers.py
@@ -0,0 +1,138 @@
+from rest_framework import serializers
+from users.models import CustomUser as User
+from djoser.serializers import UserCreateSerializer, UserSerializer
+
+from processing.models import Resume, Vacancy, JobMatching
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'patronymic_name',
+            'lastname',
+            'password',
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User.objects.create(**validated_data)
+        user.is_active = True
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomUserSerializer(UserSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'patronymic_name',
+            'surname',
+        ]
+
+
+class ResumeCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resume
+        fields = [
+            'id',
+            'first_name',
+            'patronymic_name',
+            'surname',
+            'jobs',
+            'skills',
+            'resume_file',
+        ]
+
+    def create(self, validated_data):
+        return Resume.objects.create(**validated_data)
+
+
+class ResumeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Resume
+        fields = [
+            'id',
+            'first_name',
+            'patronymic_name',
+            'surname',
+            'jobs',
+            'skills',
+            'resume_file',
+            'created_at',
+        ]
+
+
+class VacancyCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = [
+            'id',
+            'name',
+            'description',
+            'required_skills',
+        ]
+
+    def create(self, validated_data):
+        return Vacancy.objects.create(**validated_data)
+
+
+class VacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = [
+            'id',
+            'name',
+            'description',
+            'required_skills',
+            'created_at',
+            'updated_at',
+        ]
+
+
+class JobMatchingCreateSerializer(serializers.ModelSerializer):
+    vacancy = serializers.PrimaryKeyRelatedField(read_only=True)
+    resume = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = JobMatching
+        fields = [
+            'id',
+            'user',
+            'vacancy',
+            'resume',
+            'match',
+            'shortage',
+        ]
+
+    def create(self, validated_data):
+        return JobMatching.objects.create(**validated_data)
+
+
+class JobMatchingSerializer(serializers.ModelSerializer):
+    vacancy = serializers.PrimaryKeyRelatedField(read_only=True)
+    resume = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = JobMatching
+        fields = [
+            'id',
+            'name',
+            'description',
+            'required_skills',
+            'created_at',
+            'updated_at',
+        ]
